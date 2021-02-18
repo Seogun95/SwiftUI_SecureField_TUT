cdd1d91546b2b587ccd1cdeb6d53e2d69bd5dd91
SeonJungKim <ksj083895@gmail.com>
2021-02-18 19:36:39 +0500

    SecureField 튜토리얼

--- SwiftUI_SecureField_TUT/ContentView.swift
+++ SwiftUI_SecureField_TUT/ContentView.swift
@@ -1,16 +1,43 @@
-//
-//  ContentView.swift
-//  SwiftUI_SecureField_TUT
-//
-//  Created by 김선중 on 2021/02/18.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var showPassword = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.blue
+                .edgesIgnoringSafeArea(.all)  //SafeArea 까지 색을 채움
+            VStack {
+                HStack { //아이디
+                    Image(systemName: "person")
+                        .foregroundColor(.secondary)
+                    TextField("Username",
+                              text: $username)
+                }   .padding()
+                .background(Capsule().fill(Color.white))
+                
+                HStack {  //비밀번호
+                    Image(systemName: "lock")
+                        .foregroundColor(.secondary)
+                    if showPassword {
+                        TextField("Password", text: $password)
+                    } else {
+                        SecureField("Password", text: $password)  //보안텍스트
+                    }
+                      //눈버튼을 눌렀을때, showPassword가 true로 됨
+                    Button(action: { self.showPassword.toggle()}) {
+                        
+                        Image(systemName: "eye")
+                            .foregroundColor(.secondary)
+                    }
+                }   .padding()
+                .background(Capsule().fill(Color.white))
+            }   .padding()
+        }
     }
 }
 
