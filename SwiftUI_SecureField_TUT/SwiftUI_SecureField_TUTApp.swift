518531e383b413fee2156800b8c9df7b6216bf14
SeonJungKim <ksj083895@gmail.com>
2021-02-18 19:35:51 +0500

    Initial Commit

---
+++ SwiftUI_SecureField_TUT/SwiftUI_SecureField_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SecureField_TUTApp.swift
+//  SwiftUI_SecureField_TUT
+//
+//  Created by 김선중 on 2021/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SecureField_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
